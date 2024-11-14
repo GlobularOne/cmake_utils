e899e6ada48fd7bceb7f6ea2995d5372f710dd00
Kamyar <139503563+GlobularOne@users.noreply.github.com>
2024-11-14 13:15:44 +0330

    Installation.cmake: Improve formatting

--- Installation.cmake
+++ Installation.cmake
@@ -2,18 +2,18 @@ include(GNUInstallDirs)
 
 # Install executable
 function(install_executable target)
-  install(TARGETS ${target} RUNTIME)
+    install(TARGETS ${target} RUNTIME)
 endfunction()
 
 # Install a library, it's public headers and namelink component
 function(install_library target)
-  install(
-    TARGETS ${target}
-    LIBRARY
-    PUBLIC_HEADER NAMELINK_COMPONENT)
+    install(
+        TARGETS ${target}
+        LIBRARY
+        PUBLIC_HEADER NAMELINK_COMPONENT)
 endfunction()
 
 # Install a directory as data
 function(install_data dir)
-  install(DIRECTORY ${dir} TYPE DATA)
+    install(DIRECTORY ${dir} TYPE DATA)
 endfunction()
