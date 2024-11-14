815fd77cf39ec2ebdf8dbf7ba1d83a0c7c5d7790
Kamyar <139503563+GlobularOne@users.noreply.github.com>
2024-11-14 13:16:26 +0330

    CompilerStandards.cmake: Improve formatting and use better standards for C and C++

--- CompilerStandards.cmake
+++ CompilerStandards.cmake
@@ -1,29 +1,29 @@
 # Set C standard, defaults to C99
 function(set_c_standard)
-  if("C" IN_LIST LANGUAGES)
-    if(ARGC EQUAL 1)
-      set(C_STANDARD
-          ${ARGV0}
-          PARENT_SCOPE)
-    else()
-      set(C_STANDARD
-          99
-          PARENT_SCOPE)
-    endif()
-  endif()
+	if("C" IN_LIST LANGUAGES)
+		if(ARGC EQUAL 1)
+			set(C_STANDARD
+				${ARGV0}
+				PARENT_SCOPE)
+		else()
+			set(C_STANDARD
+				11
+				PARENT_SCOPE)
+		endif()
+	endif()
 endfunction()
 
 # Set C++ standard, defaults to C++14
 function(set_cxx_standard)
-  if("CXX" IN_LIST LANGUAGES)
-    if(ARGC EQUAL 1)
-      set(CXX_STANDARD
-          ${cxx_standard}
-          PARENT_SCOPE)
-    else()
-      set(CXX_STANDARD
-          14
-          PARENT_SCOPE)
-    endif()
-  endif()
+	if("CXX" IN_LIST LANGUAGES)
+		if(ARGC EQUAL 1)
+			set(CXX_STANDARD
+				${cxx_standard}
+				PARENT_SCOPE)
+		else()
+			set(CXX_STANDARD
+				17
+				PARENT_SCOPE)
+		endif()
+	endif()
 endfunction()
