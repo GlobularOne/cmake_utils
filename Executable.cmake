19ec267386d3ff05881a61322c114e61a080a46e
Kamyar <139503563+GlobularOne@users.noreply.github.com>
2024-11-14 13:15:55 +0330

    Executable.cmake: Improve formatting

--- Executable.cmake
+++ Executable.cmake
@@ -1,20 +1,25 @@
 # Generate an executable target, automatically discovering sources from src.
 # Accepts an optional subdirectory
+
 function(generate_executable executable_name)
-  if(ARGC EQUAL 2)
-    set(subdir ${ARGV1})
-  else()
-    set(subdir "/")
-  endif()
-  set(sources)
-  set(sources_dir ${PROJECT_SOURCE_DIR}/src/${subdir}/)
-  if("C" IN_LIST LANGUAGES)
-    file(GLOB_RECURSE c_sources "${sources_dir}/*.c")
-    list(APPEND sources ${c_sources})
-  endif()
-  if("CXX" IN_LIST LANGUAGES)
-    file(GLOB_RECURSE cpp_sources "${sources_dir}/*.cpp")
-    list(APPEND sources ${cpp_sources})
-  endif()
-  add_executable(${executable_name} ${sources})
+    if(ARGC EQUAL 2)
+        set(subdir ${ARGV1})
+    else()
+        set(subdir "/")
+    endif()
+
+    set(sources)
+    set(sources_dir ${PROJECT_SOURCE_DIR}/src/${subdir}/)
+
+    if("C" IN_LIST LANGUAGES)
+        file(GLOB_RECURSE c_sources "${sources_dir}/*.c")
+        list(APPEND sources ${c_sources})
+    endif()
+
+    if("CXX" IN_LIST LANGUAGES)
+        file(GLOB_RECURSE cpp_sources "${sources_dir}/*.cpp")
+        list(APPEND sources ${cpp_sources})
+    endif()
+
+    add_executable(${executable_name} ${sources})
 endfunction()
